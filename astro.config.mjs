c4aee8dba8392848111a19dded4804b08805ab2f
juanfelipe86 <arangojuanfelipe620@gmail.com>
2025-03-29 16:40:37 -0500

    vercel added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig, envField } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   env: {
     schema: {
       URL_BACKEND: envField.string({
@@ -15,8 +18,12 @@ export default defineConfig({
       }),
     },
   },
+
   output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
